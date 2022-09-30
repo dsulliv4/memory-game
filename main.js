f96c20b80e55f2fd4d624d06ec6041a69c3821ca
Deirdre Sullivan <drsullivan93@gmail.com>
2022-09-30 08:38:39 -0400

    card array js

---
+++ main.js
@@ -0,0 +1,50 @@
+const cardArray = [
+    {
+        name: 'fries'
+        img: 'imgs/fries.png',
+    },
+    {
+        name: 'cheeseburger'
+        img: 'imgs/cheeseburger.png',
+    },
+    {
+        name: 'hotdog'
+        img: 'imgs/hotdog.png',
+    },
+    {
+        name: 'ice-cream'
+        img: 'imgs/ice-cream.png',
+    },
+    {
+        name: 'milkshake'
+        img: 'imgs/milkshake.png',
+    },
+    {
+        name: 'pizza'
+        img: 'imgs/pizza.png',
+    },
+    {
+        name: 'fries'
+        img: 'imgs/fries.png',
+    },
+    {
+        name: 'cheeseburger'
+        img: 'imgs/cheeseburger.png',
+    },
+    {
+        name: 'hotdog'
+        img: 'imgs/hotdog.png',
+    },
+    {
+        name: 'ice-cream'
+        img: 'imgs/ice-cream.png',
+    },
+    {
+        name: 'milkshake'
+        img: 'imgs/milkshake.png',
+    },
+    {
+        name: 'pizza'
+        img: 'imgs/pizza.png',
+    },
+]
