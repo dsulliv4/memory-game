7ae3938cd9b99b51b43cc6bb4434233f2726156e
Deirdre Sullivan <drsullivan93@gmail.com>
2022-10-01 05:52:11 -0400

    create game board and display on grid

--- main.js
+++ main.js
@@ -1,50 +1,67 @@
 const cardArray = [
     {
-        name: 'fries'
-        img: 'imgs/fries.png',
+        name: 'fries',
+        img: 'imgs/fries.png'
     },
     {
-        name: 'cheeseburger'
-        img: 'imgs/cheeseburger.png',
+        name: 'cheeseburger',
+        img: 'imgs/cheeseburger.png'
     },
     {
-        name: 'hotdog'
-        img: 'imgs/hotdog.png',
+        name: 'hotdog',
+        img: 'imgs/hotdog.png'
     },
     {
-        name: 'ice-cream'
-        img: 'imgs/ice-cream.png',
+        name: 'ice-cream',
+        img: 'imgs/ice-cream.png'
     },
     {
-        name: 'milkshake'
-        img: 'imgs/milkshake.png',
+        name: 'milkshake',
+        img: 'imgs/milkshake.png'
     },
     {
-        name: 'pizza'
-        img: 'imgs/pizza.png',
+        name: 'pizza',
+        img: 'imgs/pizza.png'
     },
     {
-        name: 'fries'
-        img: 'imgs/fries.png',
+        name: 'fries',
+        img: 'imgs/fries.png'
     },
     {
-        name: 'cheeseburger'
-        img: 'imgs/cheeseburger.png',
+        name: 'cheeseburger',
+        img: 'imgs/cheeseburger.png'
     },
     {
-        name: 'hotdog'
-        img: 'imgs/hotdog.png',
+        name: 'hotdog',
+        img: 'imgs/hotdog.png'
     },
     {
-        name: 'ice-cream'
-        img: 'imgs/ice-cream.png',
+        name: 'ice-cream',
+        img: 'imgs/ice-cream.png'
     },
     {
-        name: 'milkshake'
-        img: 'imgs/milkshake.png',
+        name: 'milkshake',
+        img: 'imgs/milkshake.png'
     },
     {
-        name: 'pizza'
-        img: 'imgs/pizza.png',
-    },
+        name: 'pizza',
+        img: 'imgs/pizza.png'
+    }
 ]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+
+
+const gridDisplay = document.querySelector('#grid')
+
+function createBoard () {
+    for (let i = 0; i < 10; i++) {
+        const card = document.createElement('img')
+        card.setAttribute('src', 'imgs/blank.png')
+        card.setAttribute('data-id', i)
+        gridDisplay.appendChild(card)
+    }
+}
+
+createBoard()
